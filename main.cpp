9153aeb9415f61953d23bef42df051cfcfb99df1
caang2023119isinf0017-hash <caang.2023119isinf0017@aluno.ifpi.edu.br>
2025-10-14 12:01:50 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+int main(){
+//Leia a idade de uma pessoa e informe se ela é maior ou menor de idade.
+/*
+	int idade = 0;
+	std::cout<<"Digite sua idade: ";
+	std::cin>>idade;
+	if(idade > 17){
+		std::cout<<"Você é maior de idade.";}
+	else if(idade < 18){
+		std::cout<<"Você é menor de idade.";}
+*/
+
+
+//Escreva um programa que exiba uma contagem regressiva de 10 a 1 usando while.
+/*
+	int num = 10;
+	while (num > 0){
+		std::cout<<num<<std::endl;
+		num--;}
+*/
+
+
+//Peça ao usuário números até que ele digite 0. Mostre a soma total.
+/*
+	int num = 1;
+	int soma = 0;
+	std::cout<< "SOMA."<<std::endl;
+	while(num != 0){
+		std::cout<<"Digite um número: ";
+		std::cin>>num;
+		soma = soma+num;}
+	std::cout<<"A soma dos números digitados é: "<<soma;
+*/
+
+
+//Solicite um número e mostre sua tabuada de 1 a 10.
+/*
+	int num = 0;
+	int mult = 1;
+	std::cout<<"Digite um número e veja sua TABUADA: ";
+	std::cin>>num;
+	while(mult <= 10){
+		std::cout<<mult<<"x"<<num<<"="<<mult*num<<std::endl;
+		mult++;}
+*/
+
+
+//Leia 10 números e mostre quantos deles são positivos.
+/*
+	int num = 0;
+	int cont = 1;
+	int posi = 0;
+	while(cont <= 10){
+		std::cout<<"Digite um número: ";
+		std::cin>>num;
+		if(num >= 0){
+			std::cout<<"O número "<<num<<" é positivo"<<std::endl;
+			posi++;}
+		else if(num < 0){
+			std::cout<<"O número "<<num<<" é negativo"<<std::endl;}
+		cont++;}
+	std::cout<<"Foram digitados "<<posi<<" números POSITIVOS."<<std::endl;
+*/
+
+
+//Leia as notas até que o usuário digite uma nota negativa. Exiba a média das notas válidas.
+/*
+	int nota = 0;
+	int soma = 0;
+	int quant = 0;
+	float media = 0.0;
+	while(nota >= 0){
+		std::cout<<"Digite sua nota: ";
+		std::cin>>nota;
+		if(nota>0){
+			soma = soma+nota;
+			quant++;}}
+	media = soma/quant;
+	std::cout<<"A média das notas digitadas é: "<<media;
+*/
+
+
+//Mostre todos os pares de 1 a 50 usando um laço for.
+	
+
+
+}
